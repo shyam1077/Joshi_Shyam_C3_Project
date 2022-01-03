33513c86a41bc90b0d3529857b21b0430d20477c
shyam1077 <93772913+shyam1077@users.noreply.github.com>
2022-01-03 18:48:32 +0530

    Add files via upload

---
+++ RestaurantService.java
@@ -0,0 +1,38 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException {
+        for (int i = 0; i < restaurants.size(); i++) {
+            if (Objects.equals(restaurants.get(i).getName(), restaurantName)) {
+                return restaurants.get(i);
+            }
+        }
+        throw new restaurantNotFoundException(restaurantName + "Not Found");
+    }
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        if (restaurantToBeRemoved == null) {
+            throw new restaurantNotFoundException(restaurantName + "Not Found");
+        }
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+
+
+}
